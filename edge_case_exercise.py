0048900e9b8f215233c04b67bff07afcd32e6c82
sleepy_tomer <tomeroisman@gmail.com>
2025-11-09 15:44:52 +0200

    Improve move function with edge case handling
    Refactor move function to handle edge cases when moving left or right.

--- edge_case_exercise.py
+++ edge_case_exercise.py
@@ -1,15 +1,19 @@
+direction = input("which direction would you like to go(right/left)")
+my_list = [0,1,0,0]
 def move(my_list, direction):
-
-    # Finds the index of the one in the list
-    index_of_one = my_list.index(1)
-
-    # Move the one to the left or to the right
-    if direction == 'right':
-        my_list[index_of_one] = 0
-        my_list[index_of_one + 1] = 1
-
-    elif direction == 'left':
-        my_list[index_of_one] = 0
-        my_list[index_of_one - 1] = 1
-
-    return my_list
+  index = my_list.index(1)
+  if direction == "right":
+    if index < len(my_list) -1:
+      my_list.remove(1)
+      my_list.insert(index + 1,1)
+    else:
+      print("you are already at the end of the screen")
+  elif direction == "left":
+    if index > 0:
+      my_list.remove(1)
+      my_list.insert(index - 1,1)
+    else:
+      print("the pig is already at the end of the screen")
+  return my_list
+my_list = move(my_list,direction)
+print(my_list)
