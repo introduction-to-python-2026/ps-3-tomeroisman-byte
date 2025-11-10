6b2537d259dc68121a7f3b1f0aba7a6518f81f9d
sleepy_tomer <tomeroisman@gmail.com>
2025-11-10 11:13:16 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -3,5 +3,5 @@ def app_pi(n_terms):
   for n in range(n_terms):
     term = (-1) ** n / (2 * n +1 )
     pi_est = pi_est + term  
-    pi_est = pi_est * 4
-    return app_pi
+  pi_est = pi_est * 4
+  return app_pi
